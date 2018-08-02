6dc30839987449d7329a1ec85e12f181aad834e5
VIHAR\vihar <viharbhatt95@yahoo.in>
2018-08-02 16:52:33 -0700

    Created a Robot Card component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
+import {robots} from './robots';
 import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Hello greeting={'Welcome to React'} />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+	</div>, document.getElementById('root'));
 registerServiceWorker();
