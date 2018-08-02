14f0c5993cfc95a0675debb031422730c80c69ff
VIHAR\vihar <viharbhatt95@yahoo.in>
2018-08-02 16:08:26 -0700

    Created first React component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import Hello from './Hello';
+import 'tachyons';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<h1>Hello World</h1>, document.getElementById('root'));
+ReactDOM.render(<Hello greeting={'Welcome to React'} />, document.getElementById('root'));
 registerServiceWorker();
