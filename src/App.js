526ab057099ac317a157696db33c51b471ca8170
VIHAR\vihar <viharbhatt95@yahoo.in>
2018-08-02 21:49:53 -0700

    Styled fonts and background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 import {robots} from './robots';
+import './App.css'
 
 class App extends Component {
 	constructor() {
@@ -22,7 +23,7 @@ class App extends Component {
 				});
 		return (
 				<div className="tc">
-				<h1>RoboFriends</h1>
+				<h1 className="f1">RoboFriends</h1>
 				<SearchBox textchange={this.onTextChange}/>
 				<CardList robotObject={filteredRobots} />
 				</div>
